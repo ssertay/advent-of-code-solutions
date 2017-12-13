572c9962b7f6429457234e012d62dd3b5c608656
Sertay Sener <sertaysener@gmail.com>
2017-12-13 01:16:04 -0600

    Day 13 updated.

--- Day 13 - Packet Scanners/Day13.java
+++ Day 13 - Packet Scanners/Day13.java
@@ -1,16 +1,19 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-
 public class Day13 {
-	
+
+	public static void part1(Firewall firewall) {
+		System.out.println("Severity is: "+firewall.calculateSeverity());
+	}
+
+	public static void part2(Firewall firewall) {
+		System.out.println("Minimum delay needed is: "+firewall.calculateDelayBruteForce());
+	}
+
 	public static void main(String[] args) {
 		String fileName = "/Users/sertaysener/eclipse-workspace/Day13/src/input.txt";
 		Firewall firewall = new Firewall();
 		firewall.initializeFirewallWithFile(fileName);
-		
-		System.out.println(firewall.calculateDelayBruteForce());
+
+		part1(firewall);
+		part2(firewall);
 	}
 }
